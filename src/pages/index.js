7b7294450a59f83be25625937697468bca81cbc6
Sandeep Madavu Ramachandra <sandeepap11@gmail.com>
2023-09-17 15:18:20 +0530

    google tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
+import Script from "next/script";
 import { getSortedPostsData as getTravelPostsData } from "../../lib/travelPosts";
 import { getSortedPostsData as getBlogPostsData } from "../../lib/blogPosts";
 import Home from "../components/Home";
+
+const GA_MEASUREMENT_ID = "G-YH9RV1HJJG";
 
 export async function getStaticProps() {
   const allPostsData = getTravelPostsData().concat(getBlogPostsData());
@@ -18,7 +21,18 @@ export default function HomePage({ allPostsData }) {
       <Head>
         <title>Home | Sandeep</title>
       </Head>
-
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Home allPostsData={allPostsData} />
     </div>
   );
