21b9cbe71e77ebfad21e53f6b0a3d84962703631
Sandeep Madavu <sandeepap11@gmail.com>
2025-08-15 15:40:14 +0530

    refactor next.config.js to improve structure and maintainability

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+  },
   async rewrites() {
     return [
       {
         source: "/blog/:path*",
         destination: "https://blog-temp.netlify.app/:path*",
-        reactStrictMode: true,
-        env: {
-          SERVER_URL: process.env.SERVER_URL,
-        },
       },
     ];
   },
 };
+
+module.exports = nextConfig;
